b52820e76d80bfdfc553b7693ba54df2bfa3229a
Hitesh <hitgo17@gmail.com>
2020-02-09 03:49:13 +0530

    chore: added loader
    Signed-off-by: Hitesh <hitgo17@gmail.com>

--- app.js
+++ app.js
@@ -22,7 +22,24 @@ function myFunction(selected) {
 RandImgBtn.addEventListener("click", (e) => {
   e.preventDefault()
 
-  outputBox.html(`<div class="spinner-grow" role="status"><span class="sr-only">Loading...</span></div>`)
+  outputBox.html(`<div class="loader loader--style5 text-center" title="4">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px" y="0px" width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;"
+          xml:space="preserve">
+          <rect x="0" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+          <rect x="10" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0.2s" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+          <rect x="20" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0.4s" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+        </svg>
+      </div>`)
   $.ajax({
     url: `${baseUrl}/random-image`,
     headers: { 'Access-Control-Allow-Origin': `${baseUrl}/random-image` },
@@ -44,14 +61,31 @@ RandImgBtn.addEventListener("click", (e) => {
 // TEXT API
 TextBtn.addEventListener('click', (e) => {
   e.preventDefault()
-  outputBox.html(`<div class="spinner-grow" role="status"><span class="sr-only">Loading...</span></div>`)
+  outputBox.html(`<div class="loader loader--style5 text-center" title="4">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px" y="0px" width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;"
+          xml:space="preserve">
+          <rect x="0" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+          <rect x="10" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0.2s" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+          <rect x="20" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0.4s" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+        </svg>
+      </div>`)
   $.ajax({
-    url: `${baseUrl}/text/${modification}`,
-    headers: { 'Access-Control-Allow-Origin': `${baseUrl}/text/${modification}` },
+    url: `${baseUrl} / text / ${modification}`,
+    headers: { 'Access-Control-Allow-Origin': `${baseUrl} / text / ${modification}` },
     type: "GET",
     crossDomain: true,
     success: function (response) {
-      outputBox.html(`<p>RANDOMLY generated ${modification} by Deoxys.AI <br>${response}</p>`)
+      outputBox.html(`< p > RANDOMLY generated ${modification} by Deoxys.AI < br > ${response}</p > `)
       console.log(response)
     },
     error: function (xhr, status) {
@@ -65,19 +99,38 @@ TextBtn.addEventListener('click', (e) => {
 ImgBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
+  outputBox.html(`<div class="loader loader--style5 text-center" title="4">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px" y="0px" width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;"
+          xml:space="preserve">
+          <rect x="0" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+          <rect x="10" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0.2s" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+          <rect x="20" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0.4s" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+        </svg>
+      </div>`)
+
   var fd = new FormData();
   var files = $('#file')[0].files[0];
   fd.append('file', files);
 
   $.ajax({
-    url: `${baseUrl}/image/1`,
+    url: `${baseUrl} / image / 1`,
     type: 'post',
     data: fd,
     contentType: false,
     processData: false,
     success: function (fileName) {
       if (fileName) {
-        outputBox.html(`<img class="card-img-top" src="${baseUrl}/upload/${fileName}">`)
+        outputBox.html(`< img class= "card-img-top" src = "${baseUrl}/upload/${fileName}" > `)
       }
 
     }
