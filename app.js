9afe1697aa1f813073c9aea246937b9f5a2450c0
Kartikeya Gokhale <kgkartikeyagokhale@gmail.com>
2020-02-09 09:23:38 +0530

    fix: app.js fixed

--- app.js
+++ app.js
@@ -24,46 +24,6 @@ function myFunction(selected) {
 	modification = selected;
 }
 
-// RANDOM_IMAGE API
-RandImgBtn.addEventListener("click", e => {
-	e.preventDefault();
-
-	outputBox.html(`<div class="loader loader--style5 text-center" title="4">
-        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px" y="0px" width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;"
-          xml:space="preserve">
-          <rect x="0" y="0" width="4" height="10" fill="#333">
-            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
-              begin="0" dur="0.6s" repeatCount="indefinite" />
-          </rect>
-          <rect x="10" y="0" width="4" height="10" fill="#333">
-            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
-              begin="0.2s" dur="0.6s" repeatCount="indefinite" />
-          </rect>
-          <rect x="20" y="0" width="4" height="10" fill="#333">
-            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
-              begin="0.4s" dur="0.6s" repeatCount="indefinite" />
-          </rect>
-        </svg>
-      </div>`);
-	$.ajax({
-		url: `${baseUrl}/random-image`,
-		headers: { "Access-Control-Allow-Origin": `${baseUrl}/random-image` },
-		type: "GET",
-		crossDomain: true,
-		success: function(fileName) {
-			if (fileName) {
-				outputBox.html(
-					`<img class="card-img-top random-image" src="${baseUrl}/dcgan/resultImg/${fileName}">`
-				);
-			}
-		},
-		error: function(xhr, status) {
-			alert("error");
-		}
-	});
-});
-
 // TEXT API
 TextBtn.addEventListener("click", e => {
 	e.preventDefault();
@@ -92,11 +52,53 @@ TextBtn.addEventListener("click", e => {
 		},
 		type: "GET",
 		crossDomain: true,
+
 		success: function(response) {
 			outputBox.html(
-				`<h3> RANDOMLY generated ${modification} by Deoxys.AI</h3> <br> <p>${response}</p>`
+				`<p> RANDOMLY generated ${modification} by Deoxys.AI <br> ${response}</p>`
 			);
 			console.log(response);
+		},
+		error: function(xhr, status) {
+			alert("error");
+		}
+	});
+});
+
+// RANDOM_IMAGE API
+RandImgBtn.addEventListener("click", e => {
+	e.preventDefault();
+
+	outputBox.html(`<div class="loader loader--style5 text-center" title="4">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px" y="0px" width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;"
+          xml:space="preserve">
+          <rect x="0" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+          <rect x="10" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0.2s" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+          <rect x="20" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0.4s" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+        </svg>
+      </div>`);
+	$.ajax({
+		url: `${baseUrl}/random-image`,
+		headers: { "Access-Control-Allow-Origin": `${baseUrl}/random-image` },
+		type: "GET",
+		crossDomain: true,
+
+		success: function(fileName) {
+			if (fileName) {
+				outputBox.html(
+					`<img class="card-img-top random-image" src="${baseUrl}/dcgan/resultImg/${fileName}">`
+				);
+			}
 		},
 		error: function(xhr, status) {
 			alert("error");
